a1da6a056c3b46ad4229aab4ce14b33ad68c3152
흠흠흠 <battle0412@naver.com>
2023-01-01 19:48:39 +0900

    환경세팅 완료

---
+++ src/test/java/com/im/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.im.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
